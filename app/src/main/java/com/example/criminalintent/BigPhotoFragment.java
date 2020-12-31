7429291138245b8645fc2153a7b61dc5337ab6d0
Ivan Ershov <ivan.yershov@yandex.ru>
2020-12-31 18:36:54 +0300

    update app-release.apk

--- app/src/main/java/com/example/criminalintent/BigPhotoFragment.java
+++ app/src/main/java/com/example/criminalintent/BigPhotoFragment.java
@@ -1,7 +1,6 @@
 package com.example.criminalintent;
 
 import android.annotation.SuppressLint;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
